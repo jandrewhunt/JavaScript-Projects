03b4dc86fd9caffd6c67cd6b6a2a564bb8377326
jandrewhunt <jandrewhunt@gmail.com>
2023-09-12 14:31:59 -0700

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,6 @@
 function My_First_Function () {                                     //Defining a function and naming it
     var sentence = "This is what happens when you click good!";     //Defining a variable and giivng it a string value
     sentence += " You clicked really good.";                        //Defining a variable and giivng it a string value and using += operator
-    document.getElementById("Concatenate").innerHTML = sentence;
+    document.getElementById("Concatenate").innerHTML = sentence;    //Putting the values of the variables into
+                                                                    //the HTML elementFromPoint with the "Concatenate" id
 }
