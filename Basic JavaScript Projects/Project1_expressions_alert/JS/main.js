e879158f97aa6b4a1437813f136eb691222a5979
jandrewhunt <jandrewhunt@gmail.com>
2023-09-12 12:34:35 -0700

    basic javascript project assignment
    page 64

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var A = "\"This is still a string,\" said The Professor."; // Assigning a value to variable A
+var B = " \"And strings are very cool,\"" + " said Wayne."; // Assigning a value to variable B
+window.alert(A); // using window.alert method with var A
+document.write("If you're outside, say:" + " Yeah, Yeah."); //using document.writer method
+// and concatenating a string
+document.write(B); //using document.writer method with var B
+var Family = " Bayer-Hunt", Father = " Steve", Mother = "Cecily", Son = "Jeremy", Son = "Nathaniel"; //multiple variable in statement
+document.write(Father + Family); //using document writer and concatenating variables
+document.write(3 + 3); //an expression
